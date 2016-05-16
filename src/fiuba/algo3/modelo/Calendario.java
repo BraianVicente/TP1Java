31b1d9aa95fa27f4d6b9fe1983ba010621834fc8
BraianVicente <bvicente@fi.uba.ar>
2016-05-16 20:31:49 -0300

    Initialize repo

---
+++ src/fiuba/algo3/modelo/Calendario.java
@@ -0,0 +1,47 @@
+package fiuba.algo3.modelo;
+
+import java.util.List;
+
+public class Calendario {
+
+    public boolean existePersona(String juan) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void agregarPersona(String juan) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void agregarEvento(String estudiar_algebra, List<String> invitados, int i, int i0, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean estaOcupado(String juan, int i, int i0, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean estaOcupado(String juan, int i, int i0, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean estaOcupado(String juan, int i, int i0, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean estaOcupado(String juan, int i, int i0, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void agregarEventoSemanal(String clase_Algebra, int i, List<String> invitados, int i0, int i1, int i2, int i3) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void agregarRecurso(String proyector) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean existeRecurso(String proyector) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
