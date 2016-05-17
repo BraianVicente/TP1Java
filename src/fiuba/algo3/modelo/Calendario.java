ef7845fc3cf1f3d143b4bf23b421cf3ae8c4e629
pabloernesto <caussabon@gmail.com>
2016-05-16 21:14:49 -0300

    Calendario estaOcupado cambio de firma de metodo

--- src/fiuba/algo3/modelo/Calendario.java
+++ src/fiuba/algo3/modelo/Calendario.java
@@ -16,20 +16,10 @@ public class Calendario {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public boolean estaOcupado(String juan, int i, int i0, int i1, int i2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public boolean estaOcupado(String juan, int i, int i0, int i1, int i2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public boolean estaOcupado(String juan, int i, int i0, int i1, int i2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public boolean estaOcupado(String juan, int i, int i0, int i1, int i2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean estaOcupado(String nombre,
+        int anio, int mes, int dia, int hora)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public void agregarEventoSemanal(String clase_Algebra, int i, List<String> invitados, int i0, int i1, int i2, int i3) {
